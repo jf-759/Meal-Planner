456ae54ff6fc3c16a71b8ac53bc35453fd987d61
jf-759 <jane.fernandez@students.dominican.edu>
2025-05-04 22:42:08 -0700

    added api

---
+++ src/api/spoonacular.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const API_BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch'
+
+export const fetchRecipes = async (searchTerm = '', filters = []) => {
+    try {
+        const response = await axios.get(API_BASE_URL, {
+            params: {
+                apiKey: import.meta.env.VITE_SPOONACULAR_API_KEY,
+                query: searchTerm,
+                number: 20,
+                diet: filters.join(','),
+                addRecipeInformation: true
+            },
+        })
+        return { data: response.data.results, error: null }
+    } catch (error) {
+        console.error('Error fetching recipes:', error)
+        return {data: [], error: error.message || 'An error occured' }
+    }
+}
