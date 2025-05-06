8767224acb66518907d99cb462a14868b77d7ec2
jf-759 <jane.fernandez@students.dominican.edu>
2025-05-05 22:10:36 -0700

    removed .env from tracking

--- src/api/spoonacular.jsx
+++ src/api/spoonacular.jsx
@@ -6,7 +6,7 @@ export const fetchRecipes = async (searchTerm = '', filters = []) => {
     try {
         const response = await axios.get(API_BASE_URL, {
             params: {
-                apiKey: import.meta.env.VITE_SPOONACULAR_API_KEY,
+                apiKey: import.meta.env.SPOONACULAR_API_KEY,
                 query: searchTerm,
                 number: 20,
                 diet: filters.join(','),
